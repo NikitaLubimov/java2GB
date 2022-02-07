0602cf21ea17e045882003475d43018fafe64bcd
NikitaLubimov <ikarus11122@gmail.com>
2022-02-07 17:57:57 +0300

    hw2

--- src/ru/hw1/Main.java
+++ src/ru/hw1/Main.java
@@ -1,8 +1,48 @@
 package ru.hw1;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
+        String[][] arr = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "1", "2", "3"},
+                {"7", "4", "5", "6"}
+        };
 
+        try {
+            System.out.println(sumArray(arr));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Победа");
+    }
+
+    public static int sumArray(String[][] array) {
+        if (array.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Не верный формат данных: ", i, j);
+                }
+            }
+        }
+        return sum;
     }
 }
---
+++ src/ru/hw1/MyArraySizeException.java
@@ -0,0 +1,5 @@
+package ru.hw1;
+
+public class MyArraySizeException extends RuntimeException {
+
+}
